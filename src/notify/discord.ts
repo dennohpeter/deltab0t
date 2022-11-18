95930192949e10d339733742e7ce1edd1549aeb0
Dennoh Peter <mail@dennohpeter.com>
2022-11-18 14:40:11 +0300

    update discord and telegram

--- src/notify/discord.ts
+++ src/notify/discord.ts
@@ -1,24 +1,46 @@
 import { Client } from 'discord.js'
+import EventEmitter from 'events'
 import { config } from '../config'
 
-export class Discord {
+export class Discord extends EventEmitter {
   private client: Client
+  private ready: boolean
+  private msgQueue: { text: string; chatId: string }[]
 
   constructor() {
+    super()
     this.client = new Client({
       intents: 'DirectMessages',
     })
+    this.client.login(config.TELEGRAM_BOT_TOKEN)
+    this.ready = false
+    this.msgQueue = []
   }
 
-  async send(userId: string, message: string) {
-    await this.client.login(config.DISCORD_TOKEN)
-    let user = await this.client.users.fetch(userId)
+  init = (listenChatIdOnly = false) => {
+    this.client.on('ready', () => {
+      this.ready = true
+
+      for (let i = 0; i < this.msgQueue.length; i++) {
+        let msg = this.msgQueue[i]
+        this.sendMessage(msg.chatId, msg.text)
+      }
+      this.msgQueue = []
+    })
+  }
+
+  sendMessage = async (chatId: string, text: string) => {
+    if (!this.ready) {
+      this.msgQueue.push({ text, chatId })
+      return
+    }
+    let user = await this.client.users.fetch(chatId)
 
     if (!user) {
-      console.error(`User not found: ${userId}`)
+      console.error(`User not found: ${chatId}`)
       return
     }
 
-    user.send(message)
+    user.send(text)
   }
 }
--- src/notify/telegram.ts
+++ src/notify/telegram.ts
@@ -1,13 +1,17 @@
 import { PrismaClient } from '@prisma/client'
+import EventEmitter from 'events'
 import { Telegraf } from 'telegraf'
 import { config } from '../config'
 
-export class Telegram {
+export class Telegram extends EventEmitter {
   private bot: Telegraf
 
   constructor(public prisma: PrismaClient) {
-    this.bot = new Telegraf(config.BOT_TOKEN)
+    super()
+    this.bot = new Telegraf(config.TELEGRAM_BOT_TOKEN)
+  }
 
+  init = (listenChatIdOnly = false) => {
     this.bot.use(async (ctx, next) => {
       try {
         let { id } = ctx?.message?.from || {}
@@ -45,6 +49,35 @@ export class Telegram {
 
     this.bot.on('text', async (ctx) => {
       //   user && processUserInput(ctx, user)
+
+      let { id } = ctx?.message?.from || {}
+
+      this.emit('msg', {
+        id,
+        text: ctx.message.text,
+      })
     })
+  }
+
+  sendMessage = async (chatId: number, text: string) => {
+    await this.bot.telegram.sendMessage(chatId, this.#formatMsg(text), {
+      parse_mode: 'MarkdownV2',
+      disable_web_page_preview: true,
+    })
+  }
+
+  #formatMsg = (message: string) => {
+    return message
+      .replaceAll('_', '\\_')
+      .replaceAll('|', '\\|')
+      .replaceAll('.', '\\.')
+      .replaceAll('{', '\\{')
+      .replaceAll('}', '\\}')
+      .replaceAll('=', '\\=')
+      .replaceAll('+', '\\+')
+      .replaceAll('>', '\\>')
+      .replaceAll('<', '\\<')
+      .replaceAll('-', '\\-')
+      .replaceAll('!', '\\!')
   }
 }
